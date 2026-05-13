ad3d1ba00d2e05b1ef42a1d051ac5b69f572d47e
bardokson <bardoksan2006@gmail.com>
2026-05-13 18:30:45 +0200

    Controlador del inicio de sesion

---
+++ src/mapademo/Inicio_de_sesionController.java
@@ -0,0 +1,54 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class Inicio_de_sesionController {
+
+    @FXML
+    private DatePicker Birth;
+
+    @FXML
+    private TextField Email_reg;
+
+    @FXML
+    private Label Err_birth;
+
+    @FXML
+    private Label Err_email;
+
+    @FXML
+    private Label Err_nick;
+
+    @FXML
+    private Label Err_pass;
+
+    @FXML
+    private TextField NickName_reg;
+
+    @FXML
+    private TextField Pass_reg;
+
+    @FXML
+    void Acept_reg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Cancel_reg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Ini_ses(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Pass_specs(MouseEvent event) {
+
+    }
+
+}
